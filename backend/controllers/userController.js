4994dd0ab9eb24f6aeb50312f36ce59f8a02f70b
Cameron Candau <camcandau@gmail.com>
2023-07-14 18:34:17 -0700

    Connected sessions to autoupdate in DB

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -67,7 +67,15 @@ const loginUser = asyncHandler(async(req, res) => {
   }
 })
 
+// @desc    Get user data
+// @route   GET /api/users/me
+// @access  Private
+const getMe = asyncHandler(async (req, res) => {
+  res.status(200).json(req.user)
+})
+
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    getMe
 }
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,42 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const session = require('express-session')
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
+const MongoDBStore = require('connect-mongodb-session')(session)
+
 const port = process.env.PORT || 5000
+const MAX_AGE = 1000 * 60 * 60 * 3 // 3 hours
+
+const app = express()
 
 connectDB();
 
-const app = express()
+
+
+const MongoDBstore = new MongoDBStore({
+    uri: process.env.MONGO_URI,
+    collection: 'mySessions',
+})
+
+app.use(
+    session({
+        secret: 'CHANGEME',
+        name: 'session-id', // cookies name to be put in "key" field in postman
+        store: MongoDBstore,
+        cookie: {
+            maxAge: MAX_AGE, // this is when our cookies will expired and the session will not be valid anymore (user will be log out)
+            sameSite: false,
+
+            // to turn on just in production
+            secure: false, 
+            httpOnly: false 
+        },
+        resave: true,
+        saveUninitialized: false,
+    })
+)
+
 app.use(express.urlencoded({extended: false}));
 
 app.use('/api/users', require('./routes/userRoutes'));
